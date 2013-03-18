c99a8c59eb482931de1cb18b253cf8823d0eb8ad
Zhitao He <zhitao@sics.se>
2013-03-18 16:18:26 +0100

    Reverted changes to sky platform configs.

--- platform/sky/contiki-conf.h
+++ platform/sky/contiki-conf.h
@@ -10,13 +10,11 @@
 #endif /* PLATFORM_CONF_H */
 
 #ifndef NETSTACK_CONF_MAC
-/* #define NETSTACK_CONF_MAC     csma_driver */
-#define NETSTACK_CONF_MAC     nullmac_driver
+#define NETSTACK_CONF_MAC     csma_driver
 #endif /* NETSTACK_CONF_MAC */
 
 #ifndef NETSTACK_CONF_RDC
-/* #define NETSTACK_CONF_RDC     contikimac_driver */
-#define NETSTACK_CONF_RDC     rawrdc_driver
+#define NETSTACK_CONF_RDC     contikimac_driver
 #endif /* NETSTACK_CONF_RDC */
 
 #ifndef NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE
