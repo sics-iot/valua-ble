247ffef5c5bf7ba518a3d139233fd6f47d17d6dd
Zhitao He <zhitao@sics.se>
2013-04-30 14:45:08 +0200

    restored to master

--- platform/z1/platform-conf.h
+++ platform/z1/platform-conf.h
@@ -164,8 +164,7 @@ typedef unsigned long off_t;
  * SPI bus - CC2420 pin configuration.
  */
 
-//#define CC2420_CONF_SYMBOL_LOOP_COUNT 1302	/* 326us msp430X @ 8MHz */
-#define CC2420_CONF_SYMBOL_LOOP_COUNT 2604	/* 326us msp430X @ 16MHz */
+#define CC2420_CONF_SYMBOL_LOOP_COUNT 1302	/* 326us msp430X @ 8MHz */
 
 /* P1.2 - Input: FIFOP from CC2420 */
 #define CC2420_FIFOP_PORT(type)   P1##type
