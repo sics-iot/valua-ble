b4f423685b31f74ae165665eef7401eb720602a9
Zhitao He <zhitao@sics.se>
2015-02-17 15:50:17 +0100

    blink works

--- examples/blinky/blinky.c
+++ examples/blinky/blinky.c
@@ -12,14 +12,18 @@ PROCESS_THREAD(blinky_process, ev, data)
 
 	PROCESS_BEGIN();
 
-	printf("blinky started");
+	printf("*blinky* started\n");
 
 	while(1) {
+		// on
 		leds_on(LEDS_RED);
-		etimer_set(&et, 1 * CLOCK_SECOND);
+		etimer_set(&et, CLOCK_SECOND * 1);
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+		// off
 		leds_off(LEDS_RED);
-		etimer_reset(&et);
+		etimer_set(&et, CLOCK_SECOND / 2);
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 	}
 
 	PROCESS_END();
