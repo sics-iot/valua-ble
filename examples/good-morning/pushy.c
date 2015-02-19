e15e598b9b2c00cb73a73f240aef1e4556e99de7
Zhitao He <zhitao@sics.se>
2015-02-19 10:55:24 +0100

    minor formatting

--- examples/good-morning/pushy.c
+++ examples/good-morning/pushy.c
@@ -11,12 +11,14 @@ PROCESS_THREAD(my_process, ev, data)
 {
 	PROCESS_BEGIN();
 
-	// init button sensor
+	/* Init. button sensor */
 	SENSORS_ACTIVATE(button_sensor);
 
 	while(1) {
+		/* Wait on any event */
 		PROCESS_WAIT_EVENT();
-		if(ev == sensors_event && data == &button_sensor) {
+		if(ev == sensors_event &&
+			 data == &button_sensor) {
 			leds_toggle(LEDS_BLUE);
 			printf("Ouch, don't push me!\n");
 		}
