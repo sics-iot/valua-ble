d88c62653cac778b1405d0486b9377670e0b1349
Zhitao He <zhitao@sics.se>
2015-02-19 22:01:40 +0100

    uses PROCESS_WAIT_EVENT_UNTIL() in pushy. add two examples: pushover uses etimer to delay a printf pushya uses PROCESS_WAIT_EVENT() to capture both button and etimer events

--- examples/good-morning/pushover.c
+++ examples/good-morning/pushover.c
@@ -4,23 +4,34 @@
 
 #include <stdio.h>
 
-PROCESS(my_process, "pushy");
+PROCESS(my_process, "pushover");
 AUTOSTART_PROCESSES(&my_process);
 
 PROCESS_THREAD(my_process, ev, data)
 {
+	static struct etimer et;
+	static int n;
+
 	PROCESS_BEGIN();
 
 	/* Init. button sensor */
 	SENSORS_ACTIVATE(button_sensor);
 
 	while(1) {
-		/* Wait on button event */
+		/* Wait on button push */
 		PROCESS_WAIT_EVENT_UNTIL(
 														 ev == sensors_event &&
 														 data == &button_sensor);
 		leds_toggle(LEDS_BLUE);
-		printf("Ouch, don't push me!\n");
+		printf("Ouch, don't push me...");
+
+		/* Wait for time-out */
+		etimer_set(&et, CLOCK_SECOND * 2);
+		PROCESS_WAIT_EVENT_UNTIL(
+														 etimer_expired(&et));
+		printf("to use Contiki!\n");
+		n++;
+		printf("I've said it %d time%s%s\n", n, n>1?"s":"", n>9?"!!!":"...");
 	}
 	
 	PROCESS_END();
---
+++ examples/good-morning/pushya.c
@@ -0,0 +1,36 @@
+#include "contiki.h"
+#include "dev/leds.h"
+#include "dev/button-sensor.h"
+
+#include <stdio.h>
+
+PROCESS(my_process, "pushya");
+AUTOSTART_PROCESSES(&my_process);
+
+PROCESS_THREAD(my_process, ev, data)
+{
+	static struct etimer et;
+	static int n;
+
+	PROCESS_BEGIN();
+
+	/* Init. button sensor */
+	SENSORS_ACTIVATE(button_sensor);
+
+	while(1) {
+		/* Wait on any event */
+		PROCESS_WAIT_EVENT();
+		if(ev == sensors_event &&
+			 data == &button_sensor) {
+			leds_toggle(LEDS_BLUE);
+			printf("Ouch, don't push me...");
+			etimer_set(&et, CLOCK_SECOND * 2);
+		} else if(etimer_expired(&et)) {
+			printf("to use Contiki!\n");
+			n++;
+			printf("I've said it %d time%s%s\n", n, n>1?"s":"", n>9?"!!!":"...");
+		}
+	}
+	
+	PROCESS_END();
+}
