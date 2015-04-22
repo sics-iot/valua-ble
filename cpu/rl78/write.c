ff581ac34d307137e287278ffe5e3d667a9dc083
Zhitao He <zhitao@sics.se>
2015-04-22 12:06:00 +0200

    use uart2 instead of uart0

--- cpu/rl78/write.c
+++ cpu/rl78/write.c
@@ -34,12 +34,12 @@
 
 #include <stddef.h> // for size_t.
 
-#include "uart0.h"
+#include "uart2.h"
 #include "write.h"
 
 int write(int fd, const void *buf, size_t count) {
 	size_t n;
-	for (n=0; n<count; n++) uart0_putchar(((const char*)buf)[n]);
+	for (n=0; n<count; n++) uart2_putchar(((const char*)buf)[n]);
 	return count;
 }
 
