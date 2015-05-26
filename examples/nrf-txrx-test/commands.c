16b8da27b7dfe7b44c71424c17fd8c0bcdb33335
Zhitao He <zhitao@sics.se>
2015-05-26 23:35:31 +0200

    add nrf radio packet broadcast tx/rx example

---
+++ examples/nrf-txrx-test/commands.c
@@ -0,0 +1,538 @@
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+/* #include "net/rime/rime.h" */
+/* #include "net/rime/rimestats.h" */
+#include "dev/watchdog.h"
+/* #include "sys/node-id.h" */
+/* #include "lib/crc16.h" */
+
+#include "csi00.h"
+#include "commands.h"
+
+static const struct variable *user_vars;
+
+static void (*callback)(int v);
+
+struct field
+{
+	char ch;
+	const char *name;
+	uint8_t addr;
+	unsigned msb;
+	unsigned lsb;
+};
+
+/*---------------------------------------------------------------------------*/
+struct command
+{
+	const char ch;
+	const char *name;
+	void (*f)(void);
+};
+
+static void help(void);
+
+/*---------------------------------------------------------------------------*/
+const static struct field field_list[] = {
+	{'f', "FOO", 0xFE, 4, 0},
+};
+
+// Individual command handlers
+/* static void */
+/* reboot(void) */
+/* { */
+/* 	watchdog_reboot(); */
+/* } */
+/*---------------------------------------------------------------------------*/
+/* static void */
+/* power_up(void) */
+/* { */
+/* 	int power = cc2420_get_txpower(); */
+/* 	if (power < 31) { */
+/* 		cc2420_set_txpower(power+1); */
+/* 	} */
+/* 	iprintf("tx power = %u\n", cc2420_get_txpower()); */
+/* } */
+/* /\*---------------------------------------------------------------------------*\/ */
+/* static void */
+/* power_down(void) */
+/* { */
+/* 	int power = cc2420_get_txpower(); */
+/* 	if (power > 0) { */
+/* 		cc2420_set_txpower(power-1); */
+/* 	} */
+/* 	iprintf("tx power = %u\n", cc2420_get_txpower()); */
+/* } */
+/*---------------------------------------------------------------------------*/
+/* static void */
+/* rssi(void) */
+/* { */
+/* 	int8_t val = cc2420_rssi(); */
+/* 	uint16_t reg = getreg(CC2420_AGCCTRL); */
+/* 	unsigned lna = reg & 3; */
+/* 	unsigned vga_gain = (reg &  (0x7F<<4)) >> 4; */
+/* 	iprintf("RSSI = %d LNAMIX_GAINMODE = %u VGA_GAIN = %u\n", val, lna, vga_gain); */
+/* } */
+/*---------------------------------------------------------------------------*/
+#define RF_CH 0x05
+static void
+channel_up(void)
+{
+	uint8_t rf_ch = csi00_read(RF_CH);
+	rf_ch = (rf_ch + 1) % 128;
+	csi00_write(0x20 | RF_CH, rf_ch);
+	iprintf("RF channel: %hu\n", csi00_read(RF_CH));
+}
+/*---------------------------------------------------------------------------*/
+static void
+channel_down(void)
+{
+	uint8_t rf_ch = csi00_read(RF_CH);
+	rf_ch = (rf_ch - 1) % 128;
+	csi00_write(0x20 | RF_CH, rf_ch);
+	iprintf("RF channel: %hu\n", csi00_read(RF_CH));
+}
+/*---------------------------------------------------------------------------*/
+/* static void */
+/* view_rx_statistics(void) */
+/* { */
+/* 	iprintf("rx = %lu llrx = %lu avg_rssi = %ld[%d,%d] avg_lqi = %lu[%u,%u] pkts_seen = %d pkts_read = %d\n", */
+/* 				 rimestats.rx, */
+/* 				 rimestats.llrx, */
+/* 				 (sum_rssi - (long int)rimestats.llrx/2) / (long int)rimestats.llrx, */
+/* 				 min_rssi, max_rssi, */
+/* 				 (sum_lqi + rimestats.llrx/2) / rimestats.llrx, */
+/* 				 min_lqi, max_lqi, */
+/* 				 cc2420_packets_seen, */
+/* 				 cc2420_packets_read); */
+
+/* 	// reset stat counters */
+/* 	memset(&rimestats, 0, sizeof(rimestats)); */
+/* 	sum_lqi = 0; */
+/* 	sum_rssi = 0; */
+/* 	min_rssi = 15; */
+/* 	max_rssi = -55; */
+/* 	min_lqi = 108; */
+/* 	max_lqi = 0; */
+/* 	cc2420_packets_seen = 0; */
+/* 	cc2420_packets_read = 0; */
+/* } */
+/* /\*---------------------------------------------------------------------------*\/ */
+/* static void */
+/* view_failed_rx_statistics(void) */
+/* { */
+/* 	iprintf("bad crc = %lu bad synch = %lu too long = %lu too short = %lu\n", */
+/* 				 rimestats.badcrc,  */
+/* 				 rimestats.badsynch,  */
+/* 				 rimestats.toolong, */
+/* 				 rimestats.tooshort); */
+/* } */
+/*---------------------------------------------------------------------------*/
+/* static void */
+/* view_tx_power_level(void){ */
+/* 	iprintf("tx power = %u\n", cc2420_get_txpower()); */
+/* } */
+/*---------------------------------------------------------------------------*/
+/* High speed serial debug mode */
+// 8 modes: 0: Off (default) 1: AGC status output 2: Output ADC I/Q 3: Output down mixer I/Q 4: NA 5: NA 6: Input ADC I/Q 7: Input DAC I/Q
+/* static void */
+/* debug_hssd(void) */
+/* { */
+/* #define HSSD_SRC_MSB 12 */
+/* #define HSSD_SRC_LSB 10 */
+/* 	uint16_t reg = getreg(CC2420_IOCFG1); */
+/* 	uint16_t hssd_src = FV(reg, HSSD_SRC_MSB, HSSD_SRC_LSB); */
+/* 	hssd_src = (hssd_src + 1) % 8; */
+/* 	if(hssd_src == 0) CC2420_ENABLE_FIFOP_INT(); */
+/* 	else	CC2420_DISABLE_FIFOP_INT(); */
+
+/* 	CC2420_FIFO_PORT(DIR) &= ~BV(CC2420_FIFO_PIN); */
+/* 	reg = SETFV(reg, hssd_src, HSSD_SRC_MSB, HSSD_SRC_LSB); */
+/* 	setreg(CC2420_IOCFG1, reg); */
+/* 	reg = getreg(CC2420_IOCFG1); */
+/* 	iprintf("HSSD_SRC: %u \n", FV(reg, HSSD_SRC_MSB, HSSD_SRC_LSB)); */
+/* } */
+
+/*---------------------------------------------------------------------------*/
+/* static void */
+/* debug_analog(void) */
+/* { */
+/* #define ATESTMOD_MODE_MSB 3 */
+/* #define ATESTMOD_MODE_LSB 0 */
+/* #define ATESTMOD_PD_MSB 4 */
+/* #define ATESTMOD_PD_LSB 4 */
+/* #define ATESTMOD_START 4 // first invocation jumpstarts to mode [0,8] */
+
+/* 	uint16_t reg = getreg(CC2420_TOPTST); */
+/* 	uint16_t atestmod_pd = FV(reg, ATESTMOD_PD_MSB, ATESTMOD_PD_LSB); */
+/* 	uint16_t atestmod_mode = FV(reg, ATESTMOD_MODE_MSB, ATESTMOD_MODE_LSB); */
+
+/* 	if (atestmod_pd) { */
+/* 		// Power is down: start Atest and start from mode 0 */
+/* 		atestmod_pd = 0; */
+/* 		atestmod_mode = ATESTMOD_START; // start from mode 4 */
+/* 	} else { */
+/* 		// next mode */
+/* 		atestmod_mode = (atestmod_mode+1) % 9; */
+/* 		// Last mode: stop Atest */
+/* 		if (atestmod_mode == ATESTMOD_START) */
+/* 			atestmod_pd++; */
+/* 	} */
+
+/* 	reg = SETFV(reg, atestmod_pd, ATESTMOD_PD_MSB, ATESTMOD_PD_LSB); */
+/* 	reg = SETFV(reg, atestmod_mode, ATESTMOD_MODE_MSB, ATESTMOD_MODE_LSB); */
+/* 	setreg(CC2420_TOPTST, reg); */
+/* 	reg = getreg(CC2420_TOPTST); */
+/* 	iprintf("ATESTMOD_PD: %u ATESTMOD_MODE: %u\n", */
+/* 				 FV(reg, ATESTMOD_PD_MSB, ATESTMOD_PD_LSB), */
+/* 				 FV(reg, ATESTMOD_MODE_MSB, ATESTMOD_MODE_LSB)); */
+/* } */
+/*---------------------------------------------------------------------------*/
+/* static void */
+/* alter_syncword(void) */
+/* { */
+/* 	#define SW 4 */
+/* 	const static uint16_t syncwords[SW] = {0xA70F, 0xA7FF, 0xA700, 0xCD0F}; */
+/* 	int i; */
+/* 	uint16_t reg; */
+
+/* 	reg = getreg(CC2420_SYNCWORD); */
+/* 	for(i = 0;i < SW;i++) { */
+/* 		if(syncwords[i] == reg) { */
+/* 			reg = syncwords[(i+1) % SW]; */
+/* 			break; */
+/* 		} */
+/* 	} */
+/* 	if (i==SW) reg = syncwords[0]; */
+
+/* 	setreg(CC2420_SYNCWORD, reg); */
+/* 	reg = getreg(CC2420_SYNCWORD); */
+/* 	iprintf("Syncword: 0x%X\n", reg); */
+/* } */
+/*---------------------------------------------------------------------------*/
+/* Change my MAC address, can be used to alter ACK behaviour */
+/* static void */
+/* mac_update(void) */
+/* { */
+/* 	unsigned shortaddr; */
+/* 	CC2420_READ_RAM(&shortaddr,CC2420RAM_SHORTADDR, 2); */
+/* 	// increment higher byte and clear lower byte, must correspond to linkaddr_node_addr.u8[0] if Rime stack used */
+/* 	shortaddr = (shortaddr+(1<<8))%(8<<8) & 0xFF00; */
+/* 	CC2420_WRITE_RAM(&shortaddr,CC2420RAM_SHORTADDR, 2); */
+/* 	iprintf("16-bit MAC address: 0x%04X\n", shortaddr); */
+/* } */
+
+/*---------------------------------------------------------------------------*/
+/* Store 16 MAC address as permanent node ID in XMEM */
+/* static void */
+/* burn_node_id(void) */
+/* { */
+/* 	unsigned shortaddr; */
+/* 	unsigned id; */
+/* 	CC2420_READ_RAM(&shortaddr,CC2420RAM_SHORTADDR, 2); */
+/* 	id = (shortaddr<<8)|(shortaddr>>8); */
+/* 	node_id_burn(id); */
+/* 	node_id_restore(); */
+/* 	iprintf("node ID now set to %u\n", node_id); */
+/* } */
+//
+/* static void */
+/* read_tx_fifo(void) */
+/* { */
+/* 	uint8_t byte[128]; */
+/* 	CC2420_READ_RAM(&byte,CC2420RAM_TXFIFO, 128); */
+/* 	iprintf("TXFIFO content:\n"); */
+/* 	int i; */
+/* 	for (i = 0;i < 128;i++) { */
+/* 		iprintf("%02x", byte[i]); */
+/* 	} */
+/* 	iprintf("\n"); */
+/* } */
+
+/* static void */
+/* write_tx_fifo(void) */
+/* { */
+/* 	/\* uint8_t byte = 3; *\/ */
+/* 	/\* CC2420_WRITE_RAM(&byte,CC2420RAM_TXFIFO+10, 1); *\/ */
+/* 	/\* iprintf("TXFIFO byte one %u\n", byte); *\/ */
+/* 	uint8_t snippet[6] = {0x00, 0x00, 0xA7, 4, 0x30, 0x31}; */
+/* 	unsigned short checksum; */
+/* 	checksum = crc16_data(&snippet[4], sizeof(snippet)-4, 0); */
+/* 	/\* snippet[sizeof(snippet) - 2] = crc16>>8; *\/ */
+/* 	/\* snippet[sizeof(snippet) - 1] = crc16&0xFF; *\/ */
+/* 	CC2420_WRITE_RAM(&snippet, CC2420RAM_TXFIFO, sizeof(snippet)); */
+/* 	CC2420_WRITE_RAM(&checksum, CC2420RAM_TXFIFO+sizeof(snippet), 2); */
+/* } */
+
+/* static void */
+/* read_rx_fifo(void) */
+/* { */
+/* 	uint8_t byte[128]; */
+/* 	CC2420_READ_RAM(&byte,CC2420RAM_RXFIFO, 128); */
+/* 	iprintf("RXFIFO content:\n"); */
+/* 	int i; */
+/* 	for (i = 0;i < 128;i++) { */
+/* 		iprintf("%02x", byte[i]); */
+/* 	} */
+/* 	iprintf("\n"); */
+/* } */
+
+/*---------------------------------------------------------------------------*/
+/* Convert a hex number into 16 bit characters, MSB first */
+/* static char* */
+/* u16_to_bits(uint16_t n, char bits[]) */
+/* { */
+/* 	int i; */
+
+/* 	for(i=0;i<16;i++) { */
+/* 		bits[i] = (char)((n>>(15-i) & 1) + '0'); */
+/* 	} */
+
+/* 	bits[i] = '\0'; */
+
+/* 	return bits; */
+/* } */
+
+/* static void */
+/* show_all_registers(void) */
+/* { */
+/* 	uint16_t reg; */
+/* 	enum cc2420_register addr; */
+/* 	char bits[17]; //16 bits + '\0' */
+
+/* 	for (addr = CC2420_MAIN;addr <= CC2420_RESERVED;addr++) { */
+/* 		reg = getreg(addr); */
+/* 		iprintf("0x%02X: 0x%04X %s\n", addr, reg,	u16_to_bits(reg, bits)); */
+/* 	} */
+/* } */
+
+/* static void */
+/* status(void) */
+/* { */
+/*   uint8_t status; */
+/*   CC2420_GET_STATUS(status); */
+/* 	iprintf("Status 0x%01X\n", status); */
+/* } */
+
+/* /\* Show current battery level *\/ */
+/* /\* Vtoggle=1.25/27*(72-BATTMON_VOLTAGE) *\/ */
+/* static void */
+/* battery_level(void) */
+/* { */
+/* 	unsigned lv, lv_orig; */
+/* 	unsigned ok; */
+/* 	unsigned reg; */
+
+/* 	reg = getreg(CC2420_BATTMON); */
+/* 	lv_orig = FV(reg, 4, 0); */
+/* 	setreg(CC2420_BATTMON, SETFV(reg, 1, 5, 5)); // BATTMON_EN=1 */
+/* 	reg = getreg(CC2420_BATTMON); */
+/* 	for(lv=0;lv<32;lv++) { */
+/* 		setreg(CC2420_BATTMON, SETFV(reg, lv, 4, 0)); */
+/* 		reg = getreg(CC2420_BATTMON); */
+/* 		ok = FV(reg, 6, 6); */
+/* 		/\* iprintf("%u: %u\n", lv, ok); *\/ */
+/* 		if(ok) break; */
+/* 	} */
+
+/* 	iprintf("Battery level is between %u and %u\n", lv-1, lv); */
+/* 	setreg(CC2420_BATTMON, SETFV(reg, 0, 5, 5)); // BATTMON_EN=0 */
+/* 	setreg(CC2420_BATTMON, SETFV(reg, lv_orig, 4, 0)); // restore original monitor level */
+/* } */
+
+static const struct command command_table[] =	{
+	{'h', "Help", help},
+	/* {'e', "Reboot", reboot}, */
+	/* {'w', "TX power level", view_tx_power_level}, */
+	/* {'u', "TX power+", power_up}, */
+	/* {'d', "TX power-", power_down}, */
+	/* {'r', "RSSI", rssi}, */
+	{'+', "Channel+", channel_up},
+	{'-', "Channel-", channel_down},
+	/* {'v', "Successful pkt receptions", view_rx_statistics}, */
+	/* {'V', "Unsuccessful pkt receptions", view_failed_rx_statistics}, */
+	/* {'H', "Enter HSSD mode", debug_hssd}, */
+	/* {'A', "Enable analog test mode", debug_analog}, */
+	/* {'S', "Alter sync word", alter_syncword}, */
+	/* {'M', "MAC address update", mac_update}, */
+	/* {'L', "Show all registers", show_all_registers}, */
+	/* {'s', "CC2420 status byte", status}, */
+	/* {'b', "Battery level", battery_level}, */
+	/* {'N', "Burn node id", burn_node_id}, */
+	/* {'R', "Read TXFIFO", read_tx_fifo}, */
+	/* {'W', "Write TXFIFO", write_tx_fifo}, */
+	/* {'F', "Read RXFIFO", read_rx_fifo} */
+};
+
+/* Help message: available commands */
+static void
+help(void)
+{
+	const struct command *cmd;
+	const struct field *fp;
+	iprintf("Single character commands:\n");
+	for(cmd=command_table; cmd < command_table + sizeof(command_table)/sizeof(struct command); cmd++)
+		iprintf("%c %s\n", cmd->ch, cmd->name);
+
+	iprintf("---------\n");
+	iprintf("nRF field update commands <cmd name width(bits)>:\n");
+	for(fp=field_list; fp < field_list + sizeof(field_list)/sizeof(struct field); fp++)
+		iprintf("%c %s %u\n", fp->ch, fp->name, fp->msb - fp->lsb +1);
+}
+/*-----------------------------------------------------------------------------*/
+static void
+exec_command(char c)
+{
+	const struct command *ptr = &command_table[0];
+	for(;ptr<command_table+sizeof(command_table)/sizeof(struct command);ptr++) {
+		if(c == ptr->ch) {
+			ptr->f();
+			return;
+		}
+	}
+}
+
+/* static unsigned */
+/* hexstr_to_unsigned(const char *s) */
+/* { */
+/* 	unsigned n,m; */
+
+/* 	for(n=0;(*s>='0' && *s<='9')||(*s>='a' && *s<='f')||(*s>='A' && *s<='F');s++) { */
+/* 		if(*s>='0' && *s<='9') {m=*s-'0';} */
+/* 		else if(*s>='a' && *s<='f') {m=*s-'a'+10;} */
+/* 		else {m=*s-'A'+10;} */
+/* 		n=n*16+m; */
+/* 	} */
+
+/* 	return n; */
+/* } */
+
+/* static void */
+/* print_reg(const char* hex_str) */
+/* { */
+/* 	uint16_t reg; */
+/* 	enum cc2420_register addr; */
+/* 	char bits[17]; //16 bits + '\0' */
+	
+/* 	addr = hexstr_to_unsigned(hex_str); */
+/*   if(addr >= CC2420_MAIN && addr <= CC2420_RESERVED) { */
+/* 		reg = getreg(addr); */
+/* 		iprintf("0x%02X: 0x%04X %s\n", addr, reg,	u16_to_bits(reg, bits)); */
+/* 	} else if(addr < CC2420_foo) { */
+/* 		iprintf("Strobe 0x%02X\n", addr); */
+/* 		CC2420_STROBE(addr); */
+/* 	} else { */
+/* 		iprintf("Unknown register: 0x%s\n", hex_str); */
+/* 	} */
+/* } */
+
+void
+do_command(char *s)
+{
+	static char last_cmd[2];
+	static char last_last_cmd[2];
+
+	/* update command history */
+	if(s[0] == '\0') {s = &last_cmd[0];}
+	else {
+		if(s[0] == '~') {strncpy(s, last_last_cmd, sizeof(last_last_cmd));}
+		strncpy(last_last_cmd, last_cmd, sizeof(last_last_cmd));
+		strncpy(last_cmd, s, sizeof(last_cmd));
+	}
+
+	if(s[0] >= '0' && s[0] <= '9' && callback != NULL)	{callback(s[0] - '0');}
+	else if (s[0]==s[1]) {exec_command(s[0]);}
+	else if((s[0]=='+' || s[0]=='-' || s[0]=='*' || s[0]=='/' || s[0]=='<' || s[0]=='>' || s[0]=='^' || s[0]=='\'')) {var_update(s[0], s[1]);}
+	/* else if(s[1]=='+' || s[1]=='-' || s[1]=='<' || s[1]=='>' || s[1]=='\0') {field_update(s[0], s[1]);} */
+	/* else if(s[0]=='x') {print_reg(&s[1]);} */
+}
+
+void
+commands_set_callback(void (*f)(int))
+{
+	callback = f;
+}
+
+/*-----------------------------------------------------------------------------*/ 
+// Generic user variable update operations
+void
+var_update(char op, char var)
+{
+	if(!user_vars) return;
+
+	const struct variable *vp = user_vars;
+	uint8_t *u8;
+	uint16_t *u16;
+	uint32_t *u32;
+
+	while(vp->ch != '\0') {
+		if(vp->ch == var) {
+			switch(vp->width) {
+			case 1:
+				u8 = &(vp->n->u8);
+				OP(*u8, op);
+				*u8 = (*u8) % (vp->ceiling - vp->floor + 1);
+				iprintf("%s=%u\n", vp->long_name, *u8);
+				break;
+			case 2:
+				u16 = &(vp->n->u16);
+				OP(*u16, op);
+				*u16 = (*u16) % (vp->ceiling - vp->floor + 1);
+				iprintf("%s=%u\n", vp->long_name, *u16);
+				break;
+			case 4:
+				u32 = &(vp->n->u32);
+				OP(*u32, op);
+				*u32 = (*u32) % (vp->ceiling - vp->floor + 1);
+				iprintf("%s=%lu\n", vp->long_name, *u32);
+				break;
+			default:;
+			}
+		}
+		vp++;
+	}
+}
+
+/*-----------------------------------------------------------------------------*/ 
+// Generic CC2420 field update operations
+/* void */
+/* field_update(char c, char op) */
+/* { */
+/* 	const struct field *fp; */
+/* 	unsigned reg, fv; */
+	
+/* 	for(fp=&field_list[0]; fp < &field_list[0] + sizeof(field_list)/sizeof(struct field); fp++) { */
+/* 		if(fp->ch == c) { */
+/* 			reg = getreg(fp->addr); */
+/* 			fv = FV(reg, fp->msb, fp->lsb); */
+/* 			/\* skip update when no op *\/ */
+/* 			if(op != '\0') { */
+/* 				OP(fv, op); */
+/* 				fv = fv % (0x1<<(fp->msb - fp->lsb +1)); */
+/* 				reg = SETFV(reg, fv, fp->msb, fp->lsb); */
+/* 				setreg(fp->addr, reg); */
+/* 				fv = FV(getreg(fp->addr), fp->msb, fp->lsb); */
+/* 			} */
+/* 			iprintf("%s=%u\n", fp->name, fv); */
+/* 			return; */
+/* 		} */
+/* 	} */
+/* } */
+
+/* Initialize pointer to user variables */
+void
+commands_set_user_vars(const struct variable *vars)
+{
+	if(!user_vars) iprintf("User variables initialized\n");
+	else iprintf("User variables updated\n");
+	user_vars = vars;
+
+	// debug info: variables command, name and width
+	const struct variable *vp;
+	iprintf("Cmd Variable      Width\n");
+	for(vp=user_vars;vp->ch != '\0';vp++) {
+		iprintf("%c   %s      %d\n", vp->ch, vp->long_name, vp->width);
+	}
+}
---
+++ examples/nrf-txrx-test/txrx-test.c
@@ -0,0 +1,317 @@
+/*
+ * Copyright (c) 2015, SICS Swedish ICT.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is NOT part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         An example to demonstrate basic TX/RX of the nRF radio
+ * \author
+ *         Zhitao He <zhitao@sics.se>
+ */
+
+#include "contiki.h"
+#include "platform-conf.h"
+#include "sensors.h"
+#include "button-sensor.h"
+#include "dev/serial-line.h"
+#include "csi00.h"
+#include "commands.h"
+
+#include <stdio.h> /* For printf() */
+
+extern unsigned long et_process_polled;
+/*---------------------------------------------------------------------------*/
+PROCESS(blink_process, "blink process");
+PROCESS(txrx_process, "txrx process");
+AUTOSTART_PROCESSES(&blink_process, &txrx_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(blink_process, ev, data)
+{
+	static struct etimer et;
+	
+	PROCESS_BEGIN();
+
+	iprintf("Hello, blink\n");
+
+	etimer_set(&et, CLOCK_SECOND * 1);
+
+	while(1) {
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+		LED1 = 0;
+		etimer_set(&et, CLOCK_SECOND / 4);
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+		LED1 = 1;
+		etimer_set(&et, CLOCK_SECOND / 4);
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+		LED1 = 0;
+		etimer_set(&et, CLOCK_SECOND / 4);
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+		LED1 = 1;
+		etimer_set(&et, CLOCK_SECOND / 4 * 5);
+	}
+	  
+	PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+/* nRF24L01+ commands */
+#define FLUSH_TX 0xE1
+#define FLUSH_RX 0xE2
+#define REUSE_TX_PL 0xE3
+#define NOP 0xFF
+#define R_RX_PL_WID 0x60
+#define R_RX_PAYLOAD 0x61
+#define W_TX_PAYLOAD 0xA0
+#define W_TX_PAYLOAD_NO_ACK 0xB0
+
+/* nRF24L01+ registers */
+#define CONFIG 0x00
+#define RF_CH 0x05
+#define STATUS 0x07
+#define RX_ADDR_P0 0x0A
+#define TX_ADDR 0x10
+#define DYNPD 0x1C
+#define FEATURE 0x1D
+
+/* nRF24L01+ register bits */
+#define BV(b) (1<<(b))
+// register CONFIG
+#define MASK_RX_DR BV(6)
+#define MASK_TX_DS BV(5)
+#define MASK_MAX_RT BV(4)
+#define EN_CRC BV(3)
+#define CRCO BV(2)
+#define PWR_UP BV(1)
+#define PRIM_RX BV(0)
+// register STATUS
+#define RX_DR BV(6)
+#define TX_DS BV(5)
+#define MAX_RT BV(4)
+#define RX_P_NO BV(3)|BV(2)|BV(1)
+#define TX_FULL BV(0)
+
+/* modes */
+#define PD 0
+#define RX 1
+#define TX 2
+
+static int mode;
+static uint8_t tx_addr[5] = {0x55, 0xAA, 0x56, 0xAB, 0x5A};
+static uint8_t rx_addr_p0[5] = {0x55, 0xAA, 0x56, 0xAB, 0x5A};
+
+struct mode {
+	int mode;
+	const char *display;
+	void (*handler)(int mode);
+	void (*prelog)(int mode);
+	void (*prolog)(int mode);
+	void (*et_handler)(void);
+};
+
+static void
+pd_mode(int new_mode)
+{
+	csi00_write(0x20 | CONFIG,
+	            (MASK_RX_DR|MASK_TX_DS|MASK_MAX_RT|PRIM_RX)&(~PWR_UP));
+}
+
+static void
+rx_mode(int new_mode)
+{
+	(void)csi00_strobe(FLUSH_RX);
+	csi00_write(0x20 | CONFIG,
+	            (MASK_TX_DS|MASK_MAX_RT|PWR_UP|PRIM_RX)&(~MASK_RX_DR));
+	CE = 1;
+}
+
+static void
+tx_mode(int new_mode)
+{
+	CE = 0;
+	(void)csi00_strobe(FLUSH_TX);
+	csi00_write(0x20 | CONFIG,
+	            (MASK_RX_DR|MASK_MAX_RT|PWR_UP)&(~MASK_TX_DS&~PRIM_RX));
+}
+
+const static struct mode mode_list[] = {
+	{PD, "Power down", pd_mode, NULL, NULL, NULL},
+	{RX, "RX", rx_mode, NULL, NULL, NULL},
+	/* {UNMOD, "Unmodulated carrier", unmod_mode, stop_rtimer, NULL, attack_eth}, */
+	{TX, "TX broadcast", tx_mode, NULL, NULL, NULL},
+	{-1, NULL, NULL, NULL, NULL, NULL}
+};
+
+void
+start_mode(int new_mode)
+{
+	static int last_mode;
+	const struct mode *p;
+	for (p=&mode_list[0];p->handler;p++) {
+		if(p->mode == new_mode) {
+			if(p->prelog) p->prelog(last_mode);
+			/* reset_transmitter(); */
+			p->handler(new_mode);
+			last_mode = mode;
+			mode = new_mode;
+			if(p->prolog) p->prolog(mode);
+			iprintf("%s mode\n", p->display);
+			return;
+		}
+	}
+	iprintf("Unknown mode\n");
+}
+
+PROCESS_THREAD(txrx_process, ev, data)
+{
+	/* static uint8_t  addr; */
+	/* static struct etimer et; */
+	static unsigned seqno = 0;
+	uint8_t tx_buf[32];
+	uint8_t rx_buf[32];
+	
+	PROCESS_BEGIN();
+
+	iprintf("Hello, txrx test\n");
+
+	/* Init. button sensor */
+	if (SENSORS_ACTIVATE(button_sensor) == 0) {
+		iprintf("button activation failed\n");
+		process_exit(process_current);
+	}
+
+	/* Set up digital output: CE and CSN */
+	PM0 &= ~0x3;
+	CSN = 1; // set SPI bus idle
+	CE = 0; // deactivate RX/TX
+
+	/* set up irq (INTP0) registers */
+	// falling edge trigger
+	EGP0bits.egp0 = 0;
+	EGN0bits.egn0 = 1;
+	// enable edge detection irq
+	PIF0 = 0;
+	PMK0 = 0;
+	// no need to set port direction as P137 is input-only
+
+	/* Enable all three features: dynamic payload length, payload with ack, w_tx_payload_noack command */
+	csi00_write(0x20 | FEATURE, 0x07);
+	/* Enable dynamic pl length in data pipe 0 */
+	csi00_write(0x20 | DYNPD, 0x01);
+
+	/* Set addresses */
+	int i;
+	csi00_write_message(0x20 | TX_ADDR, tx_addr, sizeof(tx_addr));
+	(void)csi00_read_message(TX_ADDR, tx_addr, sizeof(tx_addr));
+	iprintf("TX addr: 0x");
+	for(i = 0;i < sizeof(tx_addr);i++) {
+		iprintf("%02X", tx_addr[i]);
+	}
+	iprintf("\n");
+
+	csi00_write_message(0x20 | RX_ADDR_P0, rx_addr_p0, sizeof(rx_addr_p0));
+	(void)csi00_read_message(RX_ADDR_P0, rx_addr_p0, sizeof(rx_addr_p0));
+	iprintf("RX addr p0: 0x");
+	for(i = 0;i < sizeof(rx_addr_p0);i++) {
+		iprintf("%02X", rx_addr_p0[i]);
+	}
+	iprintf("\n");
+
+	/* Set RF channel */
+	csi00_write(0x20 | RF_CH, 63);
+	iprintf("RF channel: %hu\n", csi00_read(RF_CH));
+
+	/* Set radio mode  */
+	start_mode(PD);
+
+	/* Set serial command callback function */
+	commands_set_callback(start_mode);
+
+	iprintf("Press button to broadcast a packet\n");
+
+	while(1) {
+		/* Wait on button event or serial line event */
+		PROCESS_WAIT_EVENT();
+		if (ev == sensors_event && data == &button_sensor) {
+			iprintf("seqno: %u\n", seqno);
+			int n = sniprintf((char *)tx_buf, sizeof(tx_buf), "%u", seqno);
+			csi00_write_message(W_TX_PAYLOAD_NO_ACK, (uint8_t *)tx_buf, n);
+			CE = 1; // activate RX/TX
+			seqno++;
+			/* clock_delay_usec(10); */
+			/* CE = 0; */
+		} else if (ev == serial_line_event_message) {
+			do_command((char *)data);
+		} else if (ev == PROCESS_EVENT_POLL) {
+			uint8_t payload_len = csi00_read(R_RX_PL_WID);
+			(void)csi00_read_message(R_RX_PAYLOAD, rx_buf, payload_len);
+			iprintf("received (%hu): 0x", payload_len);
+			for(i = 0;i < payload_len;i++) {
+				iprintf("%02X", rx_buf[i]);
+			}
+			iprintf("\n");
+		}
+	}
+
+	PROCESS_END();
+}
+
+/*---------------------------------------------------------------------------*/
+static void
+packet_sent(void)
+{
+	iprintf("sent\n");
+}
+/*---------------------------------------------------------------------------*/
+static void
+rtx_timeout(void)
+{
+	iprintf("retransmission timeout\n");
+}
+/*---------------------------------------------------------------------------*/
+/* nRF IRQ (INTP0) ISR */
+void __attribute__ ((interrupt))
+p0_handler(void)
+{
+	PMK0 = 1;
+	uint8_t status = csi00_read(STATUS);
+	// write '1' to clear bit
+	if (status & TX_DS) {
+		csi00_write(0x20 | STATUS, status | TX_DS);
+		packet_sent();
+		CE = 0; // radio power-down mode
+	} else if (status & RX_DR) {
+		csi00_write(0x20 | STATUS, status | RX_DR);
+		process_poll(&txrx_process);
+	} else if (status & MAX_RT) {
+		csi00_write(0x20 | STATUS, status | MAX_RT);
+		rtx_timeout();
+	}
+ 	PMK0 = 0;
+}
