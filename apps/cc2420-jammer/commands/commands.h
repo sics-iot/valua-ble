8502a8806272b4aae19f5d4f71ac7e748f6a8690
Zhitao He <zhitao@sics.se>
2015-06-05 16:08:28 +0200

    command list, field list and variable list are accessed as arrays intead of by pointer (better software security)

--- apps/cc2420-jammer/commands/commands.h
+++ apps/cc2420-jammer/commands/commands.h
@@ -1,3 +1,45 @@
+/*
+ * Copyright (c) 2015, SICS Swedish ICT.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/**
+ * \defgroup serial-debug Serial debug commands
+ * @{
+ */
+
+/**
+ * \file
+ *         Debugging commands using the serial-line facility
+ * \author
+ *         Zhitao He <zhitao@sics.se>
+ */
+
 #ifndef __COMMANDS__
 #define __COMMANDS__
 
@@ -47,11 +89,22 @@ struct variable
 };
 
 void do_command(char *cmd);
-void commands_init(void (*dialpad)(int),
-                   unsigned (*getreg)(unsigned addr),
-                   void (*setreg)(unsigned addr, unsigned val),
-                   const struct command *cmd_list,
-                   const struct field *flist,
-                   const struct variable *vars);
+/**
+ * \brief      Initialize the serial debug commands.
+ * \param dialpad A user function taking a single digit number (0~9) as its parameter.
+ * \param getreg A user function that returns a register's value, given its address.
+ * \param setreg A user function that sets a register's value, given its address.
+ * \param cmd_list A pointer to an array of command structures
+ *
+ *             This function initializes the serial debug commands
+ *             with a set of user defined callback functions and data structures
+ */
+void
+commands_init(void (*dialpad)(int), 
+              unsigned (*getreg)(unsigned addr),
+              void (*setreg)(unsigned addr, unsigned val), 
+              const struct command *cmd_list, int cmd_list_len,
+              const struct field *fd_list, int fd_list_len, 
+              const struct variable *var_list, int var_list_len);
 
 #endif /*__COMMANDS__*/
